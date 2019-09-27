d98dd0cbe0c3fd631ce27277eea3571470a65e36
adisamicheal <adisamicheal20@gmail.com>
2019-09-27 16:17:48 +0100

    i am tired mehen

--- src/views/Application-form.vue
+++ src/views/Application-form.vue
@@ -6,48 +6,53 @@
             <p>Applicant Sign Up </p>
         </div>
         <div class="form-wrapper">
-          <form>
+          <form @submit.prevent="add" enctype="multipart/form-data">
+            <div class="files">
+                <input type="file" class="uploadfil" >
+                <p><img src="../assets/plus.svg" class="icon">Upload CV</p>
+            </div>
+            
             <div class="row">
                 <div class="col">
                     <label>First Name</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.firstname">
                 </div>
                 <div class="col">
                     <label>Last Name</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.lastname">
                 </div>
             </div>
 
             <div class="row rows">
                 <div class="col">
                     <label>Email</label>
-                    <input type="email" class="form-control">
+                    <input type="email" class="form-control" v-model="newUser.email">
                 </div>
                 <div class="col">
                     <label>Date of Birth</label>
-                    <input type="date" class="form-control">
+                    <input type="date" class="form-control" v-model="newUser.dob">
                 </div>
             </div>
 
             <div class="row rows">
                 <div class="col">
                     <label>Address</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.address">
                 </div>
                 <div class="col">
                     <label>University</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.university">
                 </div>
             </div>
 
             <div class="row rows">
                 <div class="col">
                     <label>Course of Study</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.course">
                 </div>
                 <div class="col">
                     <label>CGPA</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="newUser.cgpa">
                 </div>
             </div>
             <button class="btn btn-primary" type="submit">Submit</button>
@@ -58,8 +63,39 @@
 
 <script>
 export default {
-    
-}
+    name:'home',
+		data() {
+		return{
+		apiResponse:{},
+		newUser: {
+		firstname:"",
+		lastname:"",
+        email:"",
+        dob:"",
+        address:"",
+        university:"",
+        course:"",
+        cgpa:"",
+		},
+	error:{}
+
+	}
+},
+
+components:{},
+	mounted() {},
+		methods:{
+			add:function(){
+			console.log(this.newUser)
+			this.$http.post('http://localhost:3000/api/form/add',this.newUser)
+			.then(response=>{
+			console.log(response)
+			this.newUser= response.body
+            this.newUser = {};
+			})
+		}, 
+	}
+};
 </script>
 
 <style scoped>
@@ -94,10 +130,10 @@ export default {
 .form-wrapper {
     width: 80%;
     max-width: 963px;
-    height: 559px;
+    height: 643px;
     margin-left: auto;
     margin-right: auto;
-    margin-top: 45px;
+    margin-top: 50px;
     margin-bottom: 171px;
     display: block;
     background: #FFFFFF;
@@ -115,8 +151,36 @@ form {
     padding-top: 51px;
 }
 
+.files {
+    width: 20% !important;
+    max-width: 211px;
+    height: 49.97px;
+    border: 1.5px solid #2B3C4E;
+    box-sizing: border-box;
+    border-radius: 2.87205px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: -10px;
+    display: block;
+}
+.files > p {
+    font-family: Avenir;
+    font-size: 16px;
+    text-align: center;
+    padding: 10px 0;
+    color: #2B3C4E;
+  }
+.uploadfil {
+    opacity: 0;
+    position: absolute;
+    cursor: pointer;
+}
+.icon{
+    margin-right: 15px;
+}
+
 .rows {
-    margin-top: 40px;
+    margin-top: 45px;
 }
 
 label {
